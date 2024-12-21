7a78ba286c13da311cc6fccfcdccb8abf48cdb9b
unknown <pratham.das.56@gmail.com>
2024-12-21 14:28:14 +0530

    db config changes

--- server/confgis/dbConfig.js
+++ server/confgis/dbConfig.js
@@ -1,9 +1,10 @@
 const mongoose = require('mongoose');
-const { MONGODB_URI, MONGODB_NAME } = process.env;
+const { MONGODB_URI, MONGODB_NAME, MONGODB_SOURCE } = process.env;
 
 const connectDB = async () => {
     try {
-        await mongoose.connect(MONGODB_URI + MONGODB_NAME);
+        const connectionString = MONGODB_SOURCE == "ATLAS" ? MONGODB_URI : MONGODB_URI + MONGODB_NAME;
+        await mongoose.connect(connectionString);
         console.log('MongoDB connected...');
     } catch (error) {
         console.error('MongoDB connection failed:', error);
